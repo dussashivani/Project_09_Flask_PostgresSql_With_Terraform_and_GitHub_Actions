c583e4d182a7eefc08e7dfc404f119393d7e95b5
Shivani Dussa <120155961+dussashivani@users.noreply.github.com>
2026-09-04 16:41:11 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,7 +13,6 @@ provider "aws" {
   region = var.region
 }
 
-# VPC with public subnets for EC2, RDS, and ALB
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "4.0.2"
@@ -21,8 +20,16 @@ module "vpc" {
   name = "greeting-vpc"
   cidr = "10.0.0.0/16"
 
-  azs             = ["us-east-1a", "us-east-1b"]
-  public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
+  azs = [
+    "us-east-1a",
+    "us-east-1b"
+  ]
+
+  public_subnets = [
+    "10.0.1.0/24",
+    "10.0.2.0/24"
+  ]
+
   enable_dns_hostnames = true
   enable_dns_support   = true
 
