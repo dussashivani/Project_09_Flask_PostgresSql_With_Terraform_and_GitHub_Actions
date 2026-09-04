6ec50fe7a73a009d5671616f4e1269b0729ad89c
Shivani Dussa <120155961+dussashivani@users.noreply.github.com>
2026-09-04 16:44:38 +0530

    Update output.tf

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,23 +1,38 @@
-# Output for RDS PostgreSQL endpoint
 output "db_endpoint" {
-  description = "The endpoint of the PostgreSQL RDS instance"
-  value       = aws_db_instance.postgres.address
+
+  description = "PostgreSQL RDS endpoint"
+
+  value = aws_db_instance.postgres.address
 }
 
-# Output for EC2 public IP
+
 output "ec2_public_ip" {
-  description = "The public IP address of the EC2 instance"
-  value       = aws_instance.flask_ec2.public_ip
+
+  description = "EC2 public IP address"
+
+  value = aws_instance.flask_ec2.public_ip
 }
+
 
 output "ecr_repo_url" {
+
+  description = "ECR repository URL"
+
   value = aws_ecr_repository.greeting_app.repository_url
 }
 
+
 output "alb_dns" {
+
+  description = "Application Load Balancer DNS"
+
   value = aws_lb.app_alb.dns_name
 }
 
+
 output "db_name" {
+
+  description = "Database name"
+
   value = aws_db_instance.postgres.db_name
 }
