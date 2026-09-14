e1969d973fa2a517e8975fc5081f906d7d137df7
Shivani Dussa <120155961+dussashivani@users.noreply.github.com>
2026-09-14 21:09:14 +0530

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,11 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "terraform-state-python1"
-    key            = "greeting-app/terraform.tfstate"
-    region         = "us-east-1"
-    encrypt        = true
-
-    # Optional: state locking with DynamoDB
-    #dynamodb_table = "terraform-locks"
+    bucket       = "terraform-state-shivani-project9-518216637461"
+    key          = "greeting-app/terraform.tfstate"
+    region       = "us-east-1"
+    encrypt      = true
+    use_lockfile = true
   }
 }
